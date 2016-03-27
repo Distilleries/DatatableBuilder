8dc07864ebc73e34a2b0c48a4e0286ede1670534
Sébastien Muller <smuller@big-boss-studio.com>
2016-03-27 21:14:57 +0200

    Add i18n for ISO 'fr'

---
+++ src/lang/fr/datatable.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'filters'               => 'Filtres',
+    'search'                => 'Rechercher',
+    'reset'                 => 'Réinitialiser',
+    'listing'               => 'Liste',
+    'activate_to_sort_asc'  => ': activer pour trier la colonne de façon ascendante',
+    'activate_to_sort_desc' => ': activer pour trier la colonne de façon descendante',
+    'no_data'               => 'Aucune donnée disponible',
+    'info'                  => '_START_ à _END_ sur un total de _TOTAL_ éléments',
+    'infoEmpty'             => 'Auncun élément trouvé',
+    'infoFiltered'          => '(filtrés depuis _MAX_ éléments au total)',
+    'lengthMenu'            => 'Voir _MENU_ éléments',
+    'zeroRecords'           => 'Aucun résultat trouvé "',
+    'view'                  => 'Voir',
+    'edit'                  => 'Editer',
+    'remove'                => 'Effacer',
+    'yes'                   => 'Oui',
+    'no'                    => 'Non',
+    'are_you_sure'          => 'Etes-vous sûr ?',
+
+];
